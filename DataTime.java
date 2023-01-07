cf9b8d313997b1ed80176121254dae9e8da32c01
João Paulo Barbosa Anastácio <89879181+Mamede5151@users.noreply.github.com>
2023-01-07 08:43:56 -0300

    Add files via upload

--- DataTime.java
+++ DataTime.java
@@ -1,5 +1,6 @@
 package provadio.me;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DataTime {
@@ -8,6 +9,12 @@ public class DataTime {
 		// TODO Auto-generated method stub
 		Date date = new Date();
 		System.out.println(date);
+		
+		SimpleDateFormat SimpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("Data Atual em formato padrão de String " + SimpleDateFormat.format(date));
+		
+		SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println("Imprimindo em formato para banco de dados " + simpleDateFormat2.format(date));;
 	}
 
 }
