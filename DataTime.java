3287df60ec54c11d54ea3cc334a2b3cb25d6b24c
João Paulo Barbosa Anastácio <89879181+Mamede5151@users.noreply.github.com>
2023-01-07 08:38:44 -0300

    estudando datas

---
+++ DataTime.java
@@ -0,0 +1,13 @@
+package provadio.me;
+
+import java.util.Date;
+
+public class DataTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date date = new Date();
+		System.out.println(date);
+	}
+
+}
